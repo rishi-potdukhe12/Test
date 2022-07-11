24c39757d72ef17ff139af677ffb31b6be61063e
rishi <rishipotdukhe84@gmail.com>
2022-07-11 12:22:02 +0530

    mergining into

---
+++ A.java
@@ -0,0 +1,8 @@
+public class A
+{
+public static void main(String[] abc){
+System.out.println("Hello From Main Branch");
+System.out.println("Food Morning From Feature1 Branch");
+}
+
+}
---
+++ B.java
@@ -0,0 +1,7 @@
+public class B
+{
+public static void main(String[] abc){
+System.out.println("Hello From Main Branch");
+System.out.println("Bye From Main Branch");
+}
+}

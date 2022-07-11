bcf86812468e60aac41e3b58d4f116fc97377c17
rishi-potdukhe12 <108914522+rishi-potdukhe12@users.noreply.github.com>
2022-07-11 12:17:17 +0530

    Update a.java

--- a.java
+++ a.java
@@ -1,6 +1,10 @@
 public class A
 {
 public static void main(String[] abc){
-System.out.println("Hello From Main Branch");
+System.out.println("Hello From feature 1 Branch");
+}
+  {
+public static void Rush(String[] abc){
+System.out.println("Bye From feature 1 Branch");
 }
 }
